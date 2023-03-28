d0e2de8798266137da5482af738a01eb4572030d
Said Saritemur <AliSaidSaritemur@gmail.com>
2023-03-28 20:51:40 +0300

    Camp Projects Started

---
+++ src/main/java/kodlama/io/rentACar/entities/concretes/Brand.java
@@ -0,0 +1,25 @@
+package kodlama.io.rentACar.entities.concretes;
+
+public class Brand {
+
+	public Brand(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+	private int id;
+	private String name;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
